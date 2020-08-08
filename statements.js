e8f255635aa55c20951a6dc005ec56c2e1a4b3d9
whykatherine <whykatherine@gmail.com>
2020-08-09 00:06:07 +0800

    :)

---
+++ statements.js
@@ -0,0 +1,53 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#Keywords
+
+1;
+"string";
+true; false;
+[1, 2, 3];
+{here: "now"};
+null;
+
+Infinity;
+NaN;
+undefined;
+
+Object;
+Function;
+Boolean;
+Symbol;
+
+Error;
+EvalError;
+RangeError;
+ReferenceError;
+SyntaxError;
+TypeError;
+URIError;
+
+Number;
+BigInt;
+Math;
+Date;
+
+String;
+RegExp;
+
+Array;
+Int8Array;
+Uint8Array;
+Float32Array;
+BigInt64Array;
+BigUint64Array;
+
+Map;
+Set;
+
+Atomics;
+DataView;
+
+Promise;
+
+Reflect;
+Proxy;
+
+eval;
---
+++ uselessprogram.js
@@ -0,0 +1,45 @@
+// uselessprogram.js
+// Adapted from "Program Structure"
+// Chapter 2 of Eloquent JavaScript (3rd Edition)
+// by Marijn Haverbeke
+
+; 1 ;
+
+Reflect;
+
+TypeError; // stereotyping?
+EvalError; // forming opinions, establishing justice
+ReferenceError; // erasing history, repeating mistakes of history
+RangeError; RangeError; RangeError; // beyond capacity
+
+DataView;
+Array; Math;
+String; RegExp;
+Atomics;
+Error;
+
+Date;
+Promise;
+Set; Map;
+
+; 2 ;
+
+"The simplest kind of statement is an expression \
+ with a semicolon after it. \
+ It is a useless program, though. \
+ A statement stands on its own, so it amounts to something \
+ only if it affects the world. \
+ The statements in the previous example \
+ just produce the values 1 and true \
+ and then immediately throw them away. \
+ This leaves no impression on the world at all. \
+ When you run this program, nothing observable happens.";
+
+; 3 ;
+
+{statement: Proxy};
+[BigInt64Array, Proxy];
+Symbol; Proxy;
+
+undefined; Function;
+SyntaxError; Infinity;
